1264e8d3b83b916f80e828c09ca5ec90e6cb9493
Natalia Ribeiro <natilgr@hotmail.com>
2023-06-27 18:09:50 -0300

    adiciona estilo à página de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,12 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { setEmailAction, setNameAction, resetPlayerAction } from '../redux/actions/index';
 import { fetchTokenFromAPI } from '../services/triviaAPI';
+import question from '../images/question.svg';
+import question1 from '../images/question1.svg';
+import question3 from '../images/question3.svg';
+import question4 from '../images/question4.svg';
+import trivia from '../images/quiz.png';
+import './Login.css';
 
 class Login extends Component {
   state = {
@@ -63,25 +69,61 @@ class Login extends Component {
     const { history } = this.props;
 
     return (
-      <div>
-        <form onSubmit={ this.validateLogin }>
+      <div className="login-page">
+        <div className="trivia-container">
+          <img className="trivia" src={ trivia } alt="trivia" />
+        </div>
+        {/* <img
+          className="question"
+          src={ question }
+          alt="question mark"
+
+        />
+        <img
+          className="question1"
+          src={ question1 }
+          alt="question mark"
+        />
+        <img
+          className="question3"
+          src={ question3 }
+          alt="question mark"
+        />
+        <img
+          className="question4"
+          src={ question4 }
+          alt="question mark"
+        /> */}
+
+        <form className="form-container" onSubmit={ this.validateLogin }>
           <input
+            className="shadow appearance-none border
+            rounded w-full py-2 px-3 text-gray-700
+            leading-tight focus:outline-none focus:shadow-outline"
             data-testid="input-gravatar-email"
             type="email"
             name="email"
-            placeholder="Type your email here..."
+            placeholder="Qual é o seu e-mail no gravatar?"
             onInput={ this.handleChange }
           />
 
           <input
+            className="shadow appearance-none border
+            rounded w-full py-2 px-3 text-gray-700
+            leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            placeholder="Qual é o seu nome?"
             data-testid="input-player-name"
             type="text"
             name="name"
-            placeholder="Type your name here..."
             onInput={ this.handleChange }
           />
 
           <button
+            className="bg-green-600 w-full
+            hover:bg-green-400 text-white
+            font-bold py-2 px-4 border-b-4
+            border-green-900 hover:border-green-500 rounded"
             data-testid="btn-play"
             type="submit"
             disabled={ isDisabled }
@@ -89,14 +131,21 @@ class Login extends Component {
           >
             Play
           </button>
+
+          <button
+            className="bg-green-800 w-full
+            hover:bg-green-400 text-white
+            font-bold py-2 px-4 border-b-4
+            border-green-900 hover:border-green-500 rounded"
+            data-testid="btn-settings"
+            onClick={ () => history.push('/settings') }
+
+          >
+            Settings
+          </button>
+
         </form>
 
-        <button
-          data-testid="btn-settings"
-          onClick={ () => history.push('/settings') }
-        >
-          Settings
-        </button>
       </div>
     );
   }
